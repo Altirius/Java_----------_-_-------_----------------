d82e2acdcf5c886d899c02ed09f125ad94565371
mv.baranov <mv.baranov@tensor.ru>
2023-04-07 17:21:19 +0300

    Урок 5 - Задание 2

--- 5-занятие/2/Program.java
+++ 5-занятие/2/Program.java
@@ -1,53 +1,56 @@
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Program {
-	class Employee {
-		String firstName;
-		String lastName;
+    class Employee {
+        String firstName;
+        String lastName;
 
-		Employee(String firstName, String lastName) {
-			this.firstName = firstName;
-			this.lastName = lastName;
-		}
-	}
-	
-	private ArrayList<Employee> employees = new ArrayList<>();
+        Employee(String firstName, String lastName) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+    }
 
-	public static void main(String[] args) {
-		System.out.println("Немного не успела :(");
-	}
+    private ArrayList<Employee> employees = new ArrayList<>();
 
-	public void start() {
+    public static void main(String[] args) {
+        Program prog = new Program();
+        prog.start();
+    }
+
+    public void start() {
         this.fillEmployeeList();
-		this.findDuplicateNames();
-	}
+        System.out.println(this.findDuplicateNames());
+    }
 
-	private void fillEmployeeList() {
-		this.employees.add(new Employee("Иван", "Иванов"));
-		this.employees.add(new Employee("Светлана", "Петрова"));
-		this.employees.add(new Employee("Кристина", "Белова"));
-		this.employees.add(new Employee("Анна", "Мусина"));
-		this.employees.add(new Employee("Анна", "Крутова"));
-		this.employees.add(new Employee("Иван", "Юрин"));
-		this.employees.add(new Employee("Петр", "Лыков"));
-		this.employees.add(new Employee("Павел", "Чернов"));
-		this.employees.add(new Employee("Петр", "Чернышов"));
-		this.employees.add(new Employee("Мария", "Федорова"));
-		this.employees.add(new Employee("Марина", "Светлова"));
-		this.employees.add(new Employee("Мария", "Савина"));
-		this.employees.add(new Employee("Мария", "Рыкова"));
-		this.employees.add(new Employee("Марина", "Лугова"));
-		this.employees.add(new Employee("Анна", "Владимирова"));
-		this.employees.add(new Employee("Иван", "Мечников"));
-		this.employees.add(new Employee("Петр", "Петин"));
-		this.employees.add(new Employee("Иван", "Ежов"));
-	}
+    private void fillEmployeeList() {
+        this.employees.add(new Employee("Иван", "Иванов"));
+        this.employees.add(new Employee("Светлана", "Петрова"));
+        this.employees.add(new Employee("Кристина", "Белова"));
+        this.employees.add(new Employee("Анна", "Мусина"));
+        this.employees.add(new Employee("Анна", "Крутова"));
+        this.employees.add(new Employee("Иван", "Юрин"));
+        this.employees.add(new Employee("Петр", "Лыков"));
+        this.employees.add(new Employee("Павел", "Чернов"));
+        this.employees.add(new Employee("Петр", "Чернышов"));
+        this.employees.add(new Employee("Мария", "Федорова"));
+        this.employees.add(new Employee("Марина", "Светлова"));
+        this.employees.add(new Employee("Мария", "Савина"));
+        this.employees.add(new Employee("Мария", "Рыкова"));
+        this.employees.add(new Employee("Марина", "Лугова"));
+        this.employees.add(new Employee("Анна", "Владимирова"));
+        this.employees.add(new Employee("Иван", "Мечников"));
+        this.employees.add(new Employee("Петр", "Петин"));
+        this.employees.add(new Employee("Иван", "Ежов"));
+    }
 
-	private HashMap<String, Integer> findDuplicateNames() {
-		HashMap<String, Integer> result = new HashMap<>();
+    private Map<String, Integer> findDuplicateNames() {
+        Map<String, Integer> result = new HashMap<>();
 
-        for(int i = 0; i < this.employees.size(); i++) {
+        for (int i = 0; i < this.employees.size(); i++) {
             String name = this.employees.get(i).firstName;
             int repeats = 1;
             if (result.containsKey(name)) {
@@ -57,9 +60,10 @@ public class Program {
             result.put(name, repeats);
         }
 
-        return result;
-	}
+        return result.entrySet()
+            .stream()
+            .filter(repeats -> repeats.getValue() > 1)
+            .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+    }
 
-
-	
 }
