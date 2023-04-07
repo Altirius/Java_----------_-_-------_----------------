eb9610bea72c97d4bf803351106ca69635f3ab4b
mv.baranov <mv.baranov@tensor.ru>
2023-04-07 14:34:01 +0300

    Урок 5 - Задание 2

--- 5-занятие/2/Program.java
+++ 5-занятие/2/Program.java
@@ -1,5 +1,65 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Program {
+	class Employee {
+		String firstName;
+		String lastName;
+
+		Employee(String firstName, String lastName) {
+			this.firstName = firstName;
+			this.lastName = lastName;
+		}
+	}
+	
+	private ArrayList<Employee> employees = new ArrayList<>();
+
 	public static void main(String[] args) {
 		System.out.println("Немного не успела :(");
 	}
+
+	public void start() {
+        this.fillEmployeeList();
+		this.findDuplicateNames();
+	}
+
+	private void fillEmployeeList() {
+		this.employees.add(new Employee("Иван", "Иванов"));
+		this.employees.add(new Employee("Светлана", "Петрова"));
+		this.employees.add(new Employee("Кристина", "Белова"));
+		this.employees.add(new Employee("Анна", "Мусина"));
+		this.employees.add(new Employee("Анна", "Крутова"));
+		this.employees.add(new Employee("Иван", "Юрин"));
+		this.employees.add(new Employee("Петр", "Лыков"));
+		this.employees.add(new Employee("Павел", "Чернов"));
+		this.employees.add(new Employee("Петр", "Чернышов"));
+		this.employees.add(new Employee("Мария", "Федорова"));
+		this.employees.add(new Employee("Марина", "Светлова"));
+		this.employees.add(new Employee("Мария", "Савина"));
+		this.employees.add(new Employee("Мария", "Рыкова"));
+		this.employees.add(new Employee("Марина", "Лугова"));
+		this.employees.add(new Employee("Анна", "Владимирова"));
+		this.employees.add(new Employee("Иван", "Мечников"));
+		this.employees.add(new Employee("Петр", "Петин"));
+		this.employees.add(new Employee("Иван", "Ежов"));
+	}
+
+	private HashMap<String, Integer> findDuplicateNames() {
+		HashMap<String, Integer> result = new HashMap<>();
+
+        for(int i = 0; i < this.employees.size(); i++) {
+            String name = this.employees.get(i).firstName;
+            int repeats = 1;
+            if (result.containsKey(name)) {
+                repeats += result.get(name);
+            }
+
+            result.put(name, repeats);
+        }
+
+        return result;
+	}
+
+
+	
 }
